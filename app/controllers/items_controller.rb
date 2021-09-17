ce6c81ca7cee18a754b9e7c9d5d18e750c3ff13a
yutakah2 <youzhangnei76@gmail.com>
2021-09-17 17:23:35 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,10 +24,8 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user == @item.user || @item.order.present?
+    if current_user != @item.user || @item.order.present?
       redirect_to root_path
-    #else
-      #render :edit
     end
   end
 
